fa04bd20ada63c94d56f29ca33df2af1e55e69f1
martinbelmes <119888811+martinbelmes@users.noreply.github.com>
2023-10-21 17:48:23 -0300

    Update establecimientos.py

--- TP-01/establecimientos.py
+++ TP-01/establecimientos.py
@@ -11,3 +11,17 @@ dist: DataFrame = pd.read_csv(archivo_distribucion, encoding="utf8").copy()
 dist["ID"].isna().sum()  # no hay
 dist["ID"].isnull().sum()  # no hay
 dist["ID"].describe()  # vemos que son todas unicas
+
+#elimino las columnas que me sobran.
+
+dist_cols = dist.columns
+
+dist.drop(columns=["lat",
+                 "lon",
+                 "in_departamentos",
+                 "provincia_id",
+                 "empleo",
+                 "clae6",
+                 "letra",
+                 "Tipo_coordenada"],
+        inplace=True)
